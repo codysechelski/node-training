8fad2d32f4075e40557ed165b0e96dedfec9627c
Cody <codysechelski@gmail.com>
2018-04-20 16:56:55 -0500

    -

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,20 +1,39 @@
-const express = require('express');
+const express  = require('express');
+const exphbs   = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//connect to mongoose
+//mongoose.connect('mongodb://localhost:5001/vidjot-dev', {
+//  useMongoClient: true
+//})
+mongoose.connect('mongodb://localhost/my_database')
+  .then(() => console.log('db connected'))
+  .catch(err => console.log(`Error Connecting to Mongoose: ${err}`));
+
+//handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 //Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
 //About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  res.render('about');
 });
 
 //Contact Route
 app.get('/contact', (req, res) => {
-  res.send('CONTACT');
+  res.render('contact');
 });
 
 const port = 5000;
