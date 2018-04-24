21a453f468085831507e8bf5e09eeae63b7ac2f4
Cody <codysechelski@gmail.com>
2018-04-24 01:03:06 -0500

    updating app

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,22 +1,30 @@
-const express  = require('express');
-const exphbs   = require('express-handlebars');
-const mongoose = require('mongoose');
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const bodyparser = require('body-parser');
+const mongoose   = require('mongoose');
 
 const app = express();
 
 //connect to mongoose
-//mongoose.connect('mongodb://localhost:5001/vidjot-dev', {
-//  useMongoClient: true
-//})
-mongoose.connect('mongodb://localhost/my_database')
+mongoose.connect('mongodb://localhost:27017/my_database')
   .then(() => console.log('db connected'))
   .catch(err => console.log(`Error Connecting to Mongoose: ${err}`));
+
+//Load Idea Schema
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 //handlebars middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+app.use(bodyparser.json());
 
 //Index Route
 app.get('/', (req, res) => {
@@ -34,6 +42,38 @@ app.get('/about', (req, res) => {
 //Contact Route
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+//Add Idea Route
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+//process form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({
+      text     : 'Please enter a title',
+      controlId: 'title'
+    });
+  }
+  if (!req.body.details) {
+    errors.push({
+      text     : 'Please enter the details',
+      controlId: 'details'
+    });
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors : errors,
+      title  : req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send('passed');
+  }
 });
 
 const port = 5000;
