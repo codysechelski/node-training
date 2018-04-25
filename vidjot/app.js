876f9469ef88f8a0c4584f8dff1067c218aaaa4e
Cody <codysechelski@gmail.com>
2018-04-24 23:06:16 -0500

    Updating project files

--- vidjot/app.js
+++ vidjot/app.js
@@ -1,7 +1,8 @@
-const express    = require('express');
-const exphbs     = require('express-handlebars');
-const bodyparser = require('body-parser');
-const mongoose   = require('mongoose');
+const express        = require('express');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const bodyparser     = require('body-parser');
+const mongoose       = require('mongoose');
 
 const app = express();
 
@@ -26,6 +27,9 @@ app.use(bodyparser.urlencoded({
 }));
 app.use(bodyparser.json());
 
+//Method Override Middleware
+app.use(methodOverride('_method'));
+
 //Index Route
 app.get('/', (req, res) => {
   const title = 'Welcome';
@@ -44,9 +48,34 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+//idea index page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({
+      date: 'desc'
+    })
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      });
+    });
+});
+
 //Add Idea Route
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+//Edit Idea Route
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+      _id: req.params.id
+    })
+    .then(idea => {
+      res.render('ideas/edit', {
+        idea: idea
+      });
+    });
 });
 
 //process form
@@ -72,8 +101,32 @@ app.post('/ideas', (req, res) => {
       details: req.body.details
     });
   } else {
-    res.send('passed');
+    const newUser = {
+      title  : req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      });
   }
+});
+
+//Edit Form Process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+  .then(idea => {
+    idea.title = req.body.title;
+    idea.details = req.body.details;  
+
+    idea.save()
+      .then(idea => {
+        res.redirect('/ideas');
+      });
+  });
 });
 
 const port = 5000;
